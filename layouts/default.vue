35008561a5f26a13e9cea090fb57bcad38d9b0f9
Rafael Curi <rafinhacurig@gmail.com>
2024-06-11 19:18:15 -0300

    ✨ adicionado layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLoadingIndicator color="#2563eb" />
+  <Header />
+  <main class="min-h-[calc(100vh-131px)]">
+    <slot />
+    <UNotifications />
+  </main>
+  <Footer />
+</template>
