5ba991995495d6451ee30968021e6e9af1657f29
Rafael Curi <rafinhacurig@gmail.com>
2024-06-13 00:31:09 -0300

    🍱 🚧 adicionado midlleware e criado lista

---
+++ middleware/auth.global.ts
@@ -0,0 +1,12 @@
+export default defineNuxtRouteMiddleware(async to => {
+  const { loggedIn, fetch } = useUserSession()
+
+  // * Atualizando a sessão
+  await fetch()
+
+  // * Não logado tentando acessar página protegida
+  if(!loggedIn.value && ['/lista'].includes(to.fullPath)) return navigateTo('/login')
+
+  // * Logado tentando acessar login
+  if(loggedIn.value && ['/login'].includes(to.fullPath)) return navigateTo('/adm')
+})
