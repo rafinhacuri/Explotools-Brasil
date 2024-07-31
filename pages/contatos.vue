100a2d2c4d8723191abc8eab50995f308663766c
rafael <rafinhacurig@gmail.com>
2024-07-31 14:34:42 -0300

    ajuste de responsividade

--- pages/contatos.vue
+++ pages/contatos.vue
@@ -8,20 +8,22 @@ const midias = ref([
 </script>
 
 <template>
-  <div class="mx-6 flex h-screen items-center justify-center bg-gradient-to-r from-teal-400 to-violet-300">
-    <div class="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
-      <h2 class="mb-6 text-center text-2xl font-bold text-gray-800">
-        Entre em Contato
-      </h2>
-      <div v-for="{href, icon, text} in midias" :key="text">
-        <NuxtLink
-          :href="href"
-          target="_blank"
-          class="mb-4 flex items-center justify-start rounded-lg p-4 shadow-md transition-transform hover:scale-105 hover:bg-gray-100"
-        >
-          <Icon :name="icon" size="24" class="text-teal-500" />
-          <span class="ml-3 text-lg font-medium text-gray-800">{{ text }}</span>
-        </NuxtLink>
+  <div class="bg-gradient-to-r from-teal-400 to-violet-300">
+    <div class="mx-6 flex h-screen items-center justify-center">
+      <div class="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
+        <h2 class="mb-6 text-center text-2xl font-bold text-gray-800">
+          Entre em Contato
+        </h2>
+        <div v-for="{href, icon, text} in midias" :key="text">
+          <NuxtLink
+            :href="href"
+            target="_blank"
+            class="mb-4 flex items-center justify-start rounded-lg p-4 shadow-md transition-transform hover:scale-105 hover:bg-gray-100"
+          >
+            <Icon :name="icon" size="24" class="text-teal-500" />
+            <span class="ml-3 text-lg font-medium text-gray-800">{{ text }}</span>
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
