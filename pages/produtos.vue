2a14b6366771062ebfa7897c2606eb7105d34dfc
rafael <rafinhacurig@gmail.com>
2024-07-31 16:07:42 -0300

    🚀 finalizado

--- pages/produtos.vue
+++ pages/produtos.vue
@@ -51,12 +51,12 @@ const images = ref([
 
 <template>
   <section>
-    <div class=" grid items-center justify-center space-y-4 md:grid-cols-2 md:space-x-40 md:space-y-0">
+    <div class=" flex flex-col items-center justify-center space-y-4 md:flex-row md:space-x-10 md:space-y-0">
       <div class="flex max-w-[500px] pt-3 md:ml-7 md:pt-0">
         <ClientOnly>
           <Carousel :value="images" circular :autoplay-interval="3000" :num-visible="1" :num-scroll="1" :show-indicators="false">
             <template #item="slotProps">
-              <img :src="slotProps.data.nome" :alt="slotProps.data.nome" class="my-3 size-[430px] rounded-lg object-cover md:size-[500px]">
+              <img :src="slotProps.data.nome" :alt="slotProps.data.nome" class="my-3 size-[430px] rounded-lg object-cover">
             </template>
           </Carousel>
           <template #fallback>
@@ -64,24 +64,31 @@ const images = ref([
           </template>
         </ClientOnly>
       </div>
+      <div class="flex items-center justify-center text-white">
+        <NuxtLink class="my-10 flex animate-pulse items-center justify-center rounded-full border-2 border-black bg-green-500 p-3 text-lg font-bold tracking-wide md:hidden" to="https://wa.link/vb9urm" external target="_blank">
+          <Icon name="ic:baseline-whatsapp" size="24" color="white" />
+          Solicite um orçamento
+        </NuxtLink>
+      </div>
       <div class="flex flex-col items-center">
         <NuxtLink to="/catalogos/catalogo1.pdf" download external target="_blank">
           <img src="/catalogos/catalogo1foto.jpeg" alt="Baixar Catálogo" class="mx-6 my-5 size-[250px] rounded-2xl transition-all hover:size-[280px] md:hidden">
-          <img src="/catalogos/catalogo1foto2.jpeg" alt="Baixar Catálogo" class="mx-6 my-5 hidden h-[150px]  w-[300px] rounded-2xl transition-all hover:h-[160px] hover:w-[310px] md:block">
+          <img src="/catalogos/catalogo1foto2.jpeg" alt="Baixar Catálogo" class="mx-6 my-5 hidden h-[130px]  w-[280px] rounded-2xl transition-all hover:h-[160px] hover:w-[310px] md:block">
         </NuxtLink>
         <NuxtLink to="/catalogos/catalogo2.pdf" download external target="_blank">
           <img src="/catalogos/catalogo2foto.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 size-[250px]  rounded-2xl transition-all hover:size-[280px] md:hidden">
-          <img src="/catalogos/catalogo2foto2.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 hidden  h-[150px] w-[300px] rounded-2xl transition-all hover:h-[160px] hover:w-[310px] md:block">
+          <img src="/catalogos/catalogo2foto2.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 hidden  h-[130px]  w-[280px] rounded-2xl transition-all hover:h-[160px] hover:w-[310px] md:block">
         </NuxtLink>
         <NuxtLink to="/catalogos/catalogo3.pdf" download external target="_blank">
           <img src="/catalogos/catalogo3foto.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 size-[250px] rounded-2xl transition-all hover:size-[280px] md:hidden">
-          <img src="/catalogos/catalogo3foto2.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 hidden h-[150px] w-[300px] rounded-2xl transition-all  hover:h-[160px] hover:w-[310px] md:block">
+          <img src="/catalogos/catalogo3foto2.jpeg" alt="Baixar Catálogo" class="mx-6 mb-5 hidden h-[130px]  w-[280px] rounded-2xl transition-all  hover:h-[160px] hover:w-[310px] md:block">
         </NuxtLink>
       </div>
     </div>
     <div>
       <div class="flex items-center justify-center text-white">
-        <NuxtLink class="my-10 animate-pulse rounded-full border-2 border-black bg-green-500 p-3 text-lg font-bold tracking-wide" to="https://wa.link/vb9urm" external target="_blank">
+        <NuxtLink class="my-10 flex animate-pulse items-center justify-center rounded-full border-2 border-black bg-green-500 p-3 text-lg font-bold tracking-wide" to="https://wa.link/vb9urm" external target="_blank">
+          <Icon name="ic:baseline-whatsapp" size="24" color="white" />
           Solicite um orçamento
         </NuxtLink>
       </div>
